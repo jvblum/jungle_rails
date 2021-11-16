b9b5075578dbc36efb04a9435ff9cc8fa7bb5ad2
jvblum <jvmell.skch@gmail.com>
2021-11-16 02:35:18 -0500

    implement authenticate method, case insensitive; bugfix users route

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,8 @@ class User < ActiveRecord::Base
   validates :password, presence: true, length: {minimum: 10}
 
   def self.authenticate_with_credentials email, password
-    user = User.find_by_email(email.strip)
+    # user = User.find_by_email(email.strip.downcase) # case sensitive
+    user = User.where("LOWER(email) = ?", email.strip.downcase)[0] # case insensitive search
     if (user && user == user.authenticate(password))
       return user
     end
